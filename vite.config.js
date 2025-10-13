51acc2d7d0ecc0206f57b2a6e24f47dd387d8022
Rhea Mimi Carillo <rheamimicarillo@gmail.com>
2025-10-12 21:55:49 -0700

    updated node version | updated vite.config.js const BASE -> const base | added seed submission RheaMimiCarillo.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // NOTE: if we decide to rename the repo, update this
-const BASE = '/git-basics-workshop/'
+const base = '/git-basics-workshop/'
 
 export default defineConfig({
   plugins: [react()],
