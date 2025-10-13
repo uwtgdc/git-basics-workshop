0f7b1f166c6502e29f05bd0e38f8d31721d922e7
Rhea Mimi Carillo <rheamimicarillo@gmail.com>
2025-10-12 23:43:38 -0700

    added basic documentation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
+/**
+ * The main entry point for the React application.
+ *
+ * @author Rhea Mimi Carillo <https://github.com/RheaMimiCarillo>
+ * @author UWT Game Dev Club <https://github.com/uwtgdc>
+ */
+
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Create the root element for the React application and render the App component into it.
 createRoot(document.getElementById('root')).render(<App />)
