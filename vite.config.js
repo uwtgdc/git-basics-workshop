0f74c599e86fffa388b917887473977490bd2ed3
Husky <115832834+uwtgdc@users.noreply.github.com>
2025-10-22 00:17:36 -0700

    updated header comment with authors and version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+/**
+ * @author Rhea Mimi Carillo @RheaMimiCarillo <https://github.com/RheaMimiCarillo>
+ * @author UWT Game Dev Club @uwtgdc <https://github.com/uwtgdc>
+ * @version 21 October 2025
+ */
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// NOTE: if we decide to rename the repo, update this
+// NOTE to future maintainers of this lesson: if we decide to rename the repo, update this constant to reflect the new name
 const base = '/git-basics-workshop/'
 
 export default defineConfig({
